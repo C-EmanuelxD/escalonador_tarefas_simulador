7f6ef0ec8219e0b76e42b92f66fb379390ff1893
Emanuel <emanuel132004@gmail.com>
2025-07-09 19:03:00 -0300

    Ciclos de clock corretamente passados e sinal de fim comunicado.

--- trab_escalonamento.py
+++ trab_escalonamento.py
@@ -15,59 +15,75 @@ def clock():
     ciclo = 0
     #####ABERTURA DE CONEXÕES############
     skt_clock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    skt_clock.bind((HOST, 4000))
-    skt_clock.listen(1)
-    conn, addr = skt_clock.accept() #Espera alguem conectar (Emissor deve ser a conexão)
-    print(f"Clock conectado por {addr} / Esperado: {porta_emissor}")
-    conn.setblocking(False) #Não deixa esperar mensagens e apenas checa se existem mensagens e se existem ele passa
+    skt_clock.bind((HOST, porta_clock))
+    skt_clock.listen(2)
+    print("Aguardando conexão...")
+    
     
     #Conectar ao emissor#
-    skt_emissor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    skt_emissor.connect((HOST, porta_emissor))
+    print("Aguardando conexão do emissor...")
+    conn_emissor, addr = skt_clock.accept() 
+    print(f"Clock conectado por {addr} / Esperado: {porta_emissor}")
+    conn_emissor.setblocking(False) #Não deixa esperar mensagens e apenas checa se existem mensagens e se existem ele passa
     
     #Conectar ao escalonador#
-    skt_escalonador = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    skt_escalonador.connect((HOST, porta_escalonador))
+    print("Aguardando conexão do escalonador...")
+    conn_escalonador, addr = skt_clock.accept()
+    print(f"Clock conectado por {addr} / Esperado: {porta_escalonador}")
+    conn_escalonador.setblocking(False) #Não deixa esperar mensagens e apenas checa se existem mensagens e se existem ele passa
     ###########################################
     
     while True:
         try:
-            msg = skt_clock.recv(1024)
+            msg = conn_escalonador.recv(1024)
+            print(f"Recebendo mensagem de fim de {addr}...")
             if msg and msg.decode() == 'fim':
                 print("Fim de escalonamento")
                 break
         except BlockingIOError: #Se não há dados na msg de fim só continua o processo
             pass
         
-        skt_emissor.sendall(str(ciclo).encode())
-        print(f"Ciclo de clock enviado ao emissor: {ciclo}")
+        conn_emissor.sendall(str(ciclo).encode())
             
         time.sleep(0.005)
 
-        skt_escalonador.sendall(str(ciclo).encode())
-        print(f"Ciclo de clock enviado ao escalonador: {ciclo}")
+        conn_escalonador.sendall(str(ciclo).encode())
         
             
         time.sleep(0.100)
         ciclo += 1
         
     print("Fechando conexões de sockets...")
-    conn.close()
-    skt_emissor.close()
-    skt_escalonador.close()
+    conn_emissor.close()
+    conn_escalonador.close()
     skt_clock.close()
     print("Conexões fechadas.")
     
 
 def emissor():
     s_em = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    s_em.bind((HOST, porta_emissor))
-    s_em.listen(2)
-
+    s_em.connect((HOST, porta_clock))
+    
+    while True:
+        clock = s_em.recv(8).decode()
+        print(f"Ciclo de clock: {clock} recebido no emissor!")
+        if clock == '4':
+            print("Fim: emissor")
+            break
+    
 def escalonador():
     s_es = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    s_es.bind((HOST, porta_escalonador))
-    s_es.listen(2)
+    s_es.connect((HOST, porta_clock))
+
+    while True:
+        clock = s_es.recv(8).decode()
+        print(f"Ciclo de clock: {clock} recebido no escalonador!")
+        if clock == '4':
+            print("Fim: ESCALONADOR")
+            print("Enviando sinal de fim para clock...")
+            s_es.sendall(b"fim")
+            print("Sinal enviado!")
+            break
 
 
 ##GLOBAIS##
@@ -85,7 +101,19 @@ if __name__ == "__main__":
     for tarefa in tarefas:
         tarefas[tarefa] = [int(x) for x in tarefas[tarefa]]
         
-        
+    proc_clock = multiprocessing.Process(target=clock)
+    time.sleep(0.500)
+    proc_emissor = multiprocessing.Process(target=emissor)
+    time.sleep(0.500)
+    proc_escalonador = multiprocessing.Process(target=escalonador)
     
-        
-            
+    proc_clock.start()
+    time.sleep(0.500)
+    proc_emissor.start()
+    time.sleep(0.500)
+    proc_escalonador.start()
+    
+    
+    proc_clock.join()
+    proc_emissor.join()
+    proc_escalonador.join()    
